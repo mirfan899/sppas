322118d50a8820651cbc74dc196a370bbda4d948
brigitte.bigi@gmail.com <brigitte.bigi@gmail.com>
2016-06-02 15:20:45 +0200

    updated audioinfo script

--- sppas/scripts/audioinfo.py
+++ sppas/scripts/audioinfo.py
@@ -56,16 +56,18 @@ SPPAS = os.path.join(os.path.dirname( os.path.dirname( PROGRAM ) ), "src")
 sys.path.append(SPPAS)
 
 import audiodata.io
-from audiodata.audiovolume import AudioVolume
+from audiodata.audiovolume   import AudioVolume
+from audiodata.channelvolume import ChannelVolume
+from audiodata.audioframes   import AudioFrames
 
 # ----------------------------------------------------------------------------
 # Verify and extract args:
 # ----------------------------------------------------------------------------
 
-parser = ArgumentParser(usage="%s -w file [options]" % os.path.basename(PROGRAM), description="... a script to get information about a wav file.")
+parser = ArgumentParser(usage="%s -w file [options]" % os.path.basename(PROGRAM), description="... a script to get information about an audio file.")
 
 parser.add_argument("-w", metavar="file", required=True,  help='Input audio file name')
-parser.add_argument("-f", metavar="value", default=0.01, type=float, help='Frame duration to estimate rms (default: 0.01)')
+parser.add_argument("-f", metavar="value", default=0.01, type=float, help='Frame duration to estimate rms values (default: 0.01)')
 
 if len(sys.argv) <= 1:
     sys.argv.append('-h')
@@ -80,21 +82,49 @@ audio.frameduration = args.f
 print "Audio file name:     ", args.w
 print "Duration (seconds):  ", audio.get_duration()
 print "Frame rate (Hz):     ", audio.get_framerate()
-print "Sample width (bits): ", audio.get_sampwidth()
-print "Channels:            ", audio.get_nchannels()
-print "Clipping rate (in %):"
-print "  - factor=0.2:      ", round(audio.clipping_rate(0.2)*100., 6)
-print "  - factor=0.4:      ", round(audio.clipping_rate(0.4)*100., 6)
-print "  - factor=0.6:      ", round(audio.clipping_rate(0.6)*100., 6)
-print "  - factor=0.8:      ", round(audio.clipping_rate(0.8)*100., 6)
+print "Sample width (bits): ", audio.get_sampwidth()*8
+nc = audio.get_nchannels()
+print "Number of channels:  ", nc
 
-audiovol = AudioVolume(audio, args.f)
-print "Volume:"
-print "  - min:           ", audiovol.min()
-print "  - max:           ", audiovol.max()
-print "  - mean:          ", round(audiovol.mean(),2)
-print "  - median:        ", round(audiovol.median(),2)
-print "  - stdev:         ", round(audiovol.stdev(),2)
-print "  - coefvariation: ", round(audiovol.coefvariation(),2)
+if nc == 1:
+    print "Clipping rate (in %):"
+    for i in range(2,9,2):
+        f = float(i)/10.
+        c = audio.clipping_rate( f ) * 100.
+        print "  - factor=%.1f:      %.3f"%(f,c)
+
+    audiovol = AudioVolume(audio, args.f)
+    print "Volume:"
+    print "  - min:           ", audiovol.min()
+    print "  - max:           ", audiovol.max()
+    print "  - mean:          ", round(audiovol.mean(),2)
+    print "  - median:        ", round(audiovol.median(),2)
+    print "  - stdev:         ", round(audiovol.stdev(),2)
+    print "  - coefvariation: ", round(audiovol.coefvariation(),2)
+
+else:
+
+    for n in range(nc):
+        print "Channel %d:"%(n)
+        cidx = audio.extract_channel(n)
+        channel = audio.get_channel(cidx)
+
+        # Values related to amplitude
+        frames = channel.get_frames(channel.get_nframes())
+        ca = AudioFrames(frames, channel.get_sampwidth(), 1)
+        for i in range(2,9,2):
+            f = float(i)/10.
+            c = ca.clipping_rate( f ) * 100.
+            print "  - factor=%.1f:      %.3f"%(f,c)
+
+        # RMS (=volume)
+        cv = ChannelVolume( channel )
+        print "  Volume:"
+        print "    - min:           ", cv.min()
+        print "    - max:           ", cv.max()
+        print "    - mean:          ", round(cv.mean(),2)
+        print "    - median:        ", round(cv.median(),2)
+        print "    - stdev:         ", round(cv.stdev(),2)
+        print "    - coefvariation: ", round(cv.coefvariation(),2)
 
 # ----------------------------------------------------------------------------
