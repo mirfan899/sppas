6003b2bc479d25a3c892a9ddea3ef4065238bc20
brigitte.bigi <brigitte.bigi@gmail.com>
2017-04-21 15:32:59 +0200

    Momel code cleaning

--- sppas/src/annotations/Momel/sppasmomel.py
+++ sppas/src/annotations/Momel/sppasmomel.py
@@ -1,39 +1,37 @@
-#!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
-# ---------------------------------------------------------------------------
-#            ___   __    __    __    ___
-#           /     |  \  |  \  |  \  /              Automatic
-#           \__   |__/  |__/  |___| \__             Annotation
-#              \  |     |     |   |    \             of
-#           ___/  |     |     |   | ___/              Speech
-#
-#
-#                           http://www.sppas.org/
-#
-# ---------------------------------------------------------------------------
-#            Laboratoire Parole et Langage, Aix-en-Provence, France
-#                   Copyright (C) 2011-2016  Brigitte Bigi
-#
-#                   This banner notice must not be removed
-# ---------------------------------------------------------------------------
-# Use of this software is governed by the GNU Public License, version 3.
-#
-# SPPAS is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# SPPAS is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with SPPAS. If not, see <http://www.gnu.org/licenses/>.
-#
-# ---------------------------------------------------------------------------
-# File: sppasmomel.py
-# ----------------------------------------------------------------------------
+"""
+    ..
+        ---------------------------------------------------------------------
+         ___   __    __    __    ___
+        /     |  \  |  \  |  \  /              the automatic
+        \__   |__/  |__/  |___| \__             annotation and
+           \  |     |     |   |    \             analysis
+        ___/  |     |     |   | ___/              of speech
+
+        http://www.sppas.org/
+
+        Use of this software is governed by the GNU Public License, version 3.
+
+        SPPAS is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        SPPAS is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with SPPAS. If not, see <http://www.gnu.org/licenses/>.
+
+        This banner notice must not be removed.
+
+        ---------------------------------------------------------------------
+
+    src.annotations.Momel.sppasmomel.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 
 import sys
 
@@ -44,6 +42,9 @@ from sppas.src.annotationdata.label.label import Label
 import sppas.src.annotationdata.aio
 
 from ..baseannot import sppasBaseAnnotation
+from ..annotationsexc import AnnotationOptionError
+from ..annotationsexc import EmptyInputError
+
 from .momel import Momel
 
 # ---------------------------------------------------------------------------
@@ -51,19 +52,18 @@ from .momel import Momel
 
 class sppasMomel(sppasBaseAnnotation):
     """
-    @author:       Brigitte Bigi
-    @organization: Laboratoire Parole et Langage, Aix-en-Provence, France
-    @contact:      brigitte.bigi@gmail.com
-    @license:      GPL, v3
-    @copyright:    Copyright (C) 2011-2016  Brigitte Bigi
-    @summary:      SPPAS integration of Momel.
+    :author:       Brigitte Bigi
+    :organization: Laboratoire Parole et Langage, Aix-en-Provence, France
+    :contact:      brigitte.bigi@gmail.com
+    :license:      GPL, v3
+    :copyright:    Copyright (C) 2011-2017  Brigitte Bigi
+    :summary:      SPPAS integration of Momel.
 
     """
     def __init__(self, logfile=None):
-        """
-        Constructor.
+        """ Create a new sppasMomel instance.
 
-        @param logfile (sppasLog) is a log file utility class member.
+        :param logfile: (sppasLog)
 
         """
         sppasBaseAnnotation.__init__(self, logfile)
@@ -76,10 +76,18 @@ class sppasMomel(sppasBaseAnnotation):
     # -----------------------------------------------------------------------
 
     def fix_options(self, options):
-        """
-        Fix all options.
+        """ Fix all options. Available options are:
 
-        @param options (option)
+            - lfen1
+            - hzinf
+            - hzsup
+            - maxec
+            - lfen2
+            - seuildiff_x
+            - seuildiff_y
+            - glitch
+
+        :param options: (sppasOption)
 
         """
         for opt in options:
@@ -118,20 +126,24 @@ class sppasMomel(sppasBaseAnnotation):
                 self.momel.set_option_elim_glitch(opt.get_value())
                 self._options['glitch'] = opt.get_value()
 
+            else:
+                raise AnnotationOptionError(key)
+
     # ------------------------------------------------------------------------
 
-    def set_pitch(self, inputfilename):
-        """
-        Load pitch values from a file.
+    @staticmethod
+    def set_pitch(input_filename):
+        """ Load pitch values from a file.
 
-        @return A list of pitch values (one value each 10 ms).
+        :returns: A list of pitch values (one value each 10 ms).
 
         """
-        pitch = sppas.src.annotationdata.aio.read(inputfilename)
-        pitchlist = pitch.get_pitch_list()
-        if len(pitchlist) == 0:
-            raise IOError('Error while reading '+inputfilename+'\nEmpty pitch tier.\n')
-        return pitchlist
+        pitch = sppas.src.annotationdata.aio.read(input_filename)
+        pitch_list = pitch.get_pitch_list()
+        if len(pitch_list) == 0:
+            raise EmptyInputError(name="Pitch")
+
+        return pitch_list
 
     # ------------------------------------------------------------------
 
@@ -142,17 +154,20 @@ class sppasMomel(sppasBaseAnnotation):
             output.write(str("%g" % targets[i].get_y()))
             output.write("\n")
 
-    def print_targets(self, targets, outputfile=None, trs=None):
-        """
-        Print the set of selected targets.
+    def print_targets(self, targets, output_filename=None, trs=None):
+        """ Print the set of selected targets.
+
+        :param targets:
+        :param output_filename: (str)
+        :param trs: (Transcription)
 
         """
-        if outputfile is not None:
-            if outputfile is "STDOUT":
+        if output_filename is not None:
+            if output_filename is "STDOUT":
                 output = sys.stdout
                 self.__print_tgts(targets, output)
-            elif outputfile.lower().endswith('momel') is True:
-                output = open(outputfile, "w")
+            elif output_filename.lower().endswith('momel') is True:
+                output = open(output_filename, "w")
                 self.__print_tgts(targets, output)
                 output.close()
 
@@ -160,43 +175,43 @@ class sppasMomel(sppasBaseAnnotation):
             # Attention: time in targets is in milliseconds!
             tier = trs.NewTier(name="Momel")
             for i in range(len(targets)):
+                _time = targets[i].get_x() * (0.001*self.PAS_TRAME)
+                _label = str("%d" % (targets[i].get_y()))
                 try:
-                    _time  = targets[i].get_x() * (0.001*self.PAS_TRAME)
-                    _label = str("%d" % (targets[i].get_y()))
                     tier.Append(Annotation(TimePoint(_time), Label(_label)))
                 except Exception:
                     if self.logfile is not None:
-                        self.logfile.print_message("Ignore target: time="+str(_time)+" and value="+_label, indent=2,status=3)
+                        self.logfile.print_message("Ignore target: time=" + str(_time) + " and value="+_label, indent=2, status=3)
 
-            if outputfile is not None and outputfile.lower().endswith('.pitchtier'):
-                trsp=Transcription()
+            if output_filename is not None and output_filename.lower().endswith('.pitchtier'):
+                trsp = Transcription()
                 trsp.Add(tier)
                 try:
-                    sppas.src.annotationdata.aio.write(outputfile, trsp)
+                    sppas.src.annotationdata.aio.write(output_filename, trsp)
                 except Exception:
                     if self.logfile is not None:
-                        self.logfile.print_message("Can't write PitchTier output file.",status=-1)
+                        self.logfile.print_message("Can't write PitchTier output file.", status=-1)
             return tier
 
     # ------------------------------------------------------------------
 
-    def run(self, inputfilename, trsoutput=None, outputfile=None):
+    def run(self, input_filename, trsoutput=None, outputfile=None):
         """
         Apply momel from a pitch file.
 
         """
         self.print_options()
-        self.print_diagnosis(inputfilename)
+        self.print_diagnosis(input_filename)
 
         # Get pitch values from the input
-        pitch = self.set_pitch(inputfilename)
+        pitch = self.set_pitch(input_filename)
         # Selected values (Target points) for this set of pitch values
         targets = []
 
         # List of pitch values of one **estimated** Inter-Pausal-Unit (ipu)
         ipupitch = []
         # Number of consecutive null F0 values
-        nbzero  = 0
+        nbzero = 0
         # Current time value
         curtime = 0
         # For each f0 value of the wav file
@@ -219,9 +234,14 @@ class sppasMomel(sppasBaseAnnotation):
                         iputargets = self.momel.annotate(ipupitch)
                     except Exception as e:
                         if self.logfile is not None:
-                            self.logfile.print_message('No Momel annotation between time '+ str(ipustarttime*0.01) +" and "+ str(curtime*0.01)+" due to the following error: " +str(e),indent=2,status=-1)
+                            self.logfile.print_message('No Momel annotation between time ' +
+                                                       str(ipustarttime*0.01) +
+                                                       " and " +
+                                                       str(curtime*0.01) +
+                                                       " due to the following error: " +
+                                                       str(e), indent=2, status=-1)
                         else:
-                            print "Momel Error: " + str(e)
+                            print("Momel Error: " + str(e))
                         iputargets = []
                         pass
                     # Adjust time values in the targets
@@ -235,16 +255,20 @@ class sppasMomel(sppasBaseAnnotation):
             curtime += 1
 
         # last ipu
-        if len(ipupitch)>0 and (len(ipupitch) > nbzero):
+        iputargets = []
+        if len(ipupitch) > 0 and (len(ipupitch) > nbzero):
             try:
                 iputargets = self.momel.annotate(ipupitch)
             except Exception as e:
                 if self.logfile is not None:
-                    self.logfile.print_message('No Momel annotation between time ' + str(ipustarttime*0.01) + 
-                                               " and " + str(curtime*0.01) + " due to the following error: " +
+                    self.logfile.print_message('No Momel annotation between time ' +
+                                               str(ipustarttime*0.01) +
+                                               " and " +
+                                               str(curtime*0.01) +
+                                               " due to the following error: " +
                                                str(e), indent=2, status=-1)
                 else:
-                    print "error: " + str(e)
+                    print("error: " + str(e))
                     iputargets = []
                 pass
             ipustarttime = curtime - (len(ipupitch))
@@ -254,22 +278,19 @@ class sppasMomel(sppasBaseAnnotation):
                 iputargets[i].set_x(ipustarttime + x)
             targets = targets + iputargets
 
-
         # Print results and/or estimate INTSINT (if any)
         if trsoutput:
             trsm = Transcription("TrsMomel")
             if outputfile:
                 momeltier = self.print_targets(targets, outputfile, trs=trsm)
             else:
-                momeltier = self.print_targets(targets, outputfile=None, trs=trsm)
+                momeltier = self.print_targets(targets, output_filename=None, trs=trsm)
             if self.logfile is not None:
-                self.logfile.print_message(str(len(targets))+ " targets found.",indent=2,status=3)
+                self.logfile.print_message(str(len(targets)) + " targets found.", indent=2, status=3)
 
             momeltier.SetRadius(0.005) # because one pitch estimation each 10ms...
             sppas.src.annotationdata.aio.write(trsoutput, trsm)
         elif outputfile:
             self.print_targets(targets, outputfile, trs=None)
         else:
-            self.print_targets(targets, outputfile='STDOUT', trs=None)
-
-    # ------------------------------------------------------------------
+            self.print_targets(targets, output_filename='STDOUT', trs=None)
