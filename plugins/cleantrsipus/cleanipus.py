588a47d280bb1325603d74ed81ef6072b9b217b1
brigitte.bigi@gmail.com <brigitte.bigi@gmail.com>
2019-02-16 11:38:55 +0100

    new plugin to clean ipus: under development

---
+++ plugins/cleantrsipus/cleanipus.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+# ---------------------------------------------------------------------------
+#       Laboratoire Parole et Langage
+#
+#       Copyright (C) 2019  Brigitte Bigi
+#
+#       Use of this software is governed by the GPL, v3
+#       This banner notice must not be removed
+# ---------------------------------------------------------------------------
+#
+# this program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# this program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# ---------------------------------------------------------------------------
+# cleanipus.py
+# ---------------------------------------------------------------------------
+import os
+import sys
+import logging
+import time
+import pickle
+from argparse import ArgumentParser
+
+PROGRAM = os.path.abspath(__file__)
+SPPAS = os.getenv('SPPAS')
+if SPPAS is None:
+    SPPAS = os.path.dirname(os.path.dirname(os.path.dirname(PROGRAM)))
+
+if os.path.exists(SPPAS) is False:
+    print("ERROR: SPPAS not found.")
+    sys.exit(1)
+sys.path.append(SPPAS)
+
+from sppas import sg
+from sppas import sppasRW
+from sppas import sppasTag, sppasLabel
+from sppas import sppasFindTier
+from sppas.src.ui import sppasLogSetup
+from sppas.src.config.ui import sppasAppConfig
+from sppas.src.config import symbols
+
+SIL_ORTHO = list(symbols.ortho.keys())[list(symbols.ortho.values()).index("silence")]
+
+# -----------------------------------------------------------------------
+# Verify and extract args:
+# -----------------------------------------------------------------------
+
+parser = ArgumentParser(
+    usage="%(prog)s [files] [options]",
+    description="... a program to clean IPUs",
+    add_help=True,
+    epilog="This program is a plugin of {:s}. Contact the "
+           "author at: {:s}".format(sg.__name__, sg.__contact__))
+
+group_verbose = parser.add_mutually_exclusive_group()
+
+group_verbose.add_argument(
+    "--quiet",
+    action='store_true',
+    help="Disable the verbosity")
+
+group_verbose.add_argument(
+    "--debug",
+    action='store_true',
+    help="Highest level of verbosity")
+
+parser.add_argument("-i",
+                    metavar="file",
+                    required=True,
+                    help='Input annotated file name.')
+
+# Force to print help if no argument is given then parse
+# ------------------------------------------------------
+
+if len(sys.argv) <= 1:
+    sys.argv.append('-h')
+
+args = parser.parse_args()
+
+# Redirect all messages to logging
+# --------------------------------
+
+with sppasAppConfig() as cg:
+    if not args.quiet:
+        if args.debug:
+            log_level = 0
+        else:
+            log_level = cg.log_level
+    else:
+        log_level = cg.quiet_log_level
+    lgs = sppasLogSetup(log_level)
+    lgs.stream_handler()
+
+# -----------------------------------------------------------------------
+# Read
+# -----------------------------------------------------------------------
+
+logging.info("Read {:s}".format(args.i))
+
+start_time = time.time()
+parser = sppasRW(args.i)
+trs_input = parser.read()
+end_time = time.time()
+
+# General information
+# -------------------
+logging.debug(
+    "Elapsed time for reading: {:f} seconds"
+    "".format(end_time - start_time))
+pickle_string = pickle.dumps(trs_input)
+logging.debug(
+    "Memory usage of the transcription: {:d} bytes"
+    "".format(sys.getsizeof(pickle_string)))
+
+# -----------------------------------------------------------------------
+# Work
+# -----------------------------------------------------------------------
+
+tier = sppasFindTier().transcription(trs_input)
+if tier is None:
+    logging.error('Transcription tier not found.')
+    sys.exit(1)
+
+# Replace un-transcribed ipus by silences
+# Re-index right-IPUs
+ipu = 0
+for ann in tier:
+    if ann.is_labelled() is False or ann.label_is_string() is False:
+        continue
+    if ann.get_best_tag().is_silence():
+        continue
+
+    old_label = ann.serialize_labels(separator=" ", empty="", alt=True)
+    if old_label.starts_with("ipu_"):
+        old_label = old_label[old_label.index(' '):].strip()
+
+    if len(old_label) > 0:
+        ipu += 1
+        new_labels = list()
+        new_labels.append(sppasLabel(sppasTag('ipu_%d' % ipu)))
+        new_labels.append(sppasLabel(sppasTag(old_label)))
+        ann.set_labels(new_labels)
+    else:
+        ann.set_labels(sppasLabel(sppasTag(SIL_ORTHO)))
+
+# Merge continuous silences
+i = len(tier)
+while i >= 0:
+    label = tier[i].serialize_labels()
+    i -= 1
+    c = i
+
+    while label == SIL_ORTHO:
+        label = tier[c].serialize_labels()
+        c -= 1
+
+    end_loc = tier[i].get_location().get_end().copy()
+    # continuous silences
+    if c < i:
+        while c < i:
+            tier.pop(i)
+            i -= 1
+        tier[c].get_location().set_end(end_loc)
+
+
+# -----------------------------------------------------------------------
+# Write
+# -----------------------------------------------------------------------
+
+f, e = os.path.splitext(args.i)
+output = f + "-clean." + e
+logging.info("Write {:s}".format(args.o))
+parser = sppasRW(args.o)
+start_time = time.time()
+parser.write(trs_input)
+end_time = time.time()
+
+logging.debug(
+    "Elapsed time for writing: {:f} seconds"
+    "".format(end_time - start_time))
+
+logging.info("Done.")
