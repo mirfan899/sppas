19c8c77c37430a28ad2c50cf7b460d214fcab11d
Brigitte Bigi <brigitte.bigi@lpl-aix.fr>
2019-06-11 09:56:52 +0200

    annotations: windowing system done.

--- sppas/src/annotations/Occurrences/windowing.py
+++ sppas/src/annotations/Occurrences/windowing.py
@@ -28,13 +28,22 @@
         This banner notice must not be removed.
 
         ---------------------------------------------------------------------
+
+    src.annotations.windowing.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 """
 
 import decimal
 
-from sppas import sppasTier, sppasAnnotation, sppasTypeError
+from sppas import symbols
+from sppas import sppasTier, sppasTypeError
 from sppas import sppasAnnSet
-from sppas import sppasLocation, sppasInterval
+
+# ---------------------------------------------------------------------------
+
+
+SIL_ORTHO = list(symbols.ortho.keys())[list(symbols.ortho.values()).index("silence")]
 
 # ---------------------------------------------------------------------------
 
@@ -54,7 +63,7 @@ class sppasTierWindow(object):
     """
 
     def __init__(self, tier):
-        """Create an instance of a sppaswindow.
+        """Create an instance of a sppasTierWindow.
 
         :param tier: (sppasTier) Tier to analyze
 
@@ -62,13 +71,13 @@ class sppasTierWindow(object):
         if isinstance(tier, sppasTier) is False:
             raise sppasTypeError(tier, "sppasTier")
 
+        if tier.is_disjoint():
+            raise NotImplementedError('sppasTierWindow does not support disjoint interval tiers.')
+
         self.__tier = tier
 
-        if tier.is_disjoint():
-            raise NotImplementedError('sppasWindow does not support disjoint interval tiers.')
-
     # -----------------------------------------------------------------------
-    # Utilitiy methods
+    # Utility methods
     # -----------------------------------------------------------------------
 
     @staticmethod
@@ -95,52 +104,45 @@ class sppasTierWindow(object):
                 x += decimal.Decimal(jump)
 
     # -----------------------------------------------------------------------
-    # Split methods
-    # -----------------------------------------------------------------------
 
-    def time_split(self, duration, step, delta=0.6):
-        """Return a set of annotations within a time window.
+    def search_for_annotations(self, start_time, end_time, delta=0.5, ignore=[]):
+        """Return the annotation set among the given interval.
 
-        :param duration: (float) the duration of a window
-        :param step: (float) the step duration
-        :param delta: (float) percentage of confidence for an overlapping label
-        :returns: (sppasAnnSet) Set of sppasAnnotation
-        :raises: sppasTypeError, ValueError
+        :param start_time: (int/float)
+        :param end_time: (int/float)
+        :param delta: (float) Rate of time the annotation must overlap
+        :param ignore: (list of str) List of tag contents to ignore -- currently applied only on the best tag
+        :return: (sppasAnnSet) The annotations matching all the requirements
 
         """
-        if self.__tier.is_int() is True and isinstance(duration, int) is False:
-            raise sppasTypeError(duration, "int")
-        if self.__tier.is_float() is True and isinstance(duration, float) is False:
-            raise sppasTypeError(duration, "float")
+        ann_set = sppasAnnSet()
 
-        # todo: test duration > step
+        # find annotations on the current time interval
+        anns = self.__tier.find(start_time, end_time, overlaps=True)
+        if len(anns) == 0:
+            return ann_set
 
-        ann_set_list = list()
-        start_time = self.__tier.get_first_point().get_midpoint()
-        end_time = self.__tier.get_last_point().get_midpoint()
+        # remove overlapping annotations if not overlapping enough
+        for ann in reversed(anns):
+            begin = ann.get_lowest_localization().get_midpoint()
+            dur = ann.get_location().get_best().duration().get_value()
+            overlap_point = float(begin) + (float(dur) * delta)
+            if overlap_point < float(start_time) or overlap_point > float(end_time):
+                anns.remove(ann)
 
-        for i in sppasTierWindow.drange(start_time, end_time, step):
-            #print(i, i+duration)
-            ann_set = sppasAnnSet()
-            # find annotations on the current time interval
-            anns = self.__tier.find(i, i+duration, overlaps=True)
-            if len(anns) > 0:
-                # remove overlapping annotations if not overlapping enough
-                if self.__tier.is_interval():
-                    for ann in reversed(anns):
-                        begin = ann.get_location().get_best().get_begin().get_midpoint()
-                        dur = ann.get_location().get_best().duration().get_value()
-                        overlap_point = float(begin) + (float(dur) * delta)
-                        if overlap_point < float(i) or overlap_point > float((i+duration)):
-                            anns.remove(ann)
+        if len(anns) == 0:
+            return ann_set
 
-                # add annotations in the set, add the set in the list
-                if len(anns) > 0:
-                    for ann in anns:
-                        ann_set.append(ann, list())
-                    ann_set_list.append(ann_set)
+        # check the tag content then append
+        for a in anns:
+            if a.label_is_filled():
+                tag_content = a.get_best_tag().get_typed_content()
+                if tag_content not in ignore:
+                    ann_set.append(a, list())
+            else:
+                ann_set.append(a, list())
 
-        return ann_set_list
+        return ann_set
 
     # -----------------------------------------------------------------------
 
@@ -171,9 +173,11 @@ class sppasTierWindow(object):
                 if tag.get_typed_content() in separators:
                     # tag is None or prev_ann.get_highest_localization() < ann.get_lowest_localization():
                     if end > begin:
-                        intervals.append((begin, prev_ann.get_highest_localization()))
+                        intervals.append((begin.get_midpoint(),
+                                          prev_ann.get_highest_localization().get_midpoint()))
 
-                    if tag is None or tag.get_typed_content() in separators:
+                    #if tag is None or
+                    if tag.get_typed_content() in separators:
                         begin = ann.get_highest_localization()
                     else:
                         begin = ann.get_lowest_localization()
@@ -189,16 +193,48 @@ class sppasTierWindow(object):
         if end > begin:
             ann = self.__tier[-1]
             end = ann.get_highest_localization()
-            intervals.append((begin, end))
+            intervals.append((begin.get_midpoint(), end.get_midpoint()))
 
         return intervals
 
     # -----------------------------------------------------------------------
+    # Split methods
+    # -----------------------------------------------------------------------
 
-    def anchor_split(self, duration, step, separators):
-        """Return all time intervals within a window given by separators.
+    def time_split(self, duration, step, delta=0.6):
+        """Return a set of annotations within a time window.
 
-        :param duration: (int) the duration of a window - number of intervals
+        :param duration: (float) the duration of a window
+        :param step: (float) the step duration
+        :param delta: (float) percentage of confidence for an overlapping label
+        :returns: (sppasAnnSet) Set of sppasAnnotation
+        :raises: sppasTypeError, ValueError
+
+        """
+        if self.__tier.is_int() is True and isinstance(duration, int) is False:
+            raise sppasTypeError(duration, "int")
+        if self.__tier.is_float() is True and isinstance(duration, float) is False:
+            raise sppasTypeError(duration, "float")
+
+        # todo: test duration > step
+
+        ann_set_list = list()
+        start_time = self.__tier.get_first_point().get_midpoint()
+        end_time = self.__tier.get_last_point().get_midpoint()
+
+        for i in sppasTierWindow.drange(start_time, end_time, step):
+
+            ann_set = self.search_for_annotations(i, i+duration, delta)
+            ann_set_list.append(ann_set)
+
+        return ann_set_list
+
+    # -----------------------------------------------------------------------
+
+    def anchor_split(self, duration=1, step=1, separators=[SIL_ORTHO]):
+        """Return a set of annotations within a window given by separators.
+
+        :param duration: (int) the duration of a window - number of intervals among the separators
         :param step: (int) the step duration - number of intervals
         :param separators: (list) list of separators
         :returns: (List of sppasAnnSet)
@@ -206,25 +242,20 @@ class sppasTierWindow(object):
         """
         ann_set_list = list()
         intervals = self.continuous_anchor_split(separators)
-        for i in range(0, len(intervals)+1, step):
-            ann_set = sppasAnnSet()
-            # find annotations on the current time interval
-            end = min(i+duration-1, len(intervals)-1)
-            anns = self.__tier.find(intervals[i][0],
-                                    intervals[end][1],
-                                    overlaps=True)
-            for a in anns:
-                if a.label_is_filled():
-                    tag = a.get_best_tag().get_typed_content()
-                    if tag not in separators:
-                        ann_set.append(a, list())
-                else:
-                    ann_set.append(a, list())
 
+        for i in range(0, len(intervals)+1, step):
+
+            end_idx = min(i+duration-1, len(intervals)-1)
+
+            ann_set = self.search_for_annotations(
+                intervals[i][0], intervals[end_idx][1],
+                delta=0.1, ignore=separators
+            )
             ann_set_list.append(ann_set)
 
-            if end == len(intervals)-1:
-                break
+            # Stop if end!
+            if end_idx == len(intervals)-1:
+               break
 
         return ann_set_list
 
--- sppas/src/annotations/tests/test_windowing.py
+++ sppas/src/annotations/tests/test_windowing.py
@@ -1,9 +1,45 @@
+# -*- coding: UTF-8 -*-
+"""
+    ..
+        ---------------------------------------------------------------------
+         ___   __    __    __    ___
+        /     |  \  |  \  |  \  /              the automatic
+        \__   |__/  |__/  |___| \__             annotation and
+           \  |     |     |   |    \             analysis
+        ___/  |     |     |   | ___/              of speech
+
+        http://www.sppas.org/
+
+        Use of this software is governed by the GNU Public License, version 3.
+
+        SPPAS is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        SPPAS is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with SPPAS. If not, see <http://www.gnu.org/licenses/>.
+
+        This banner notice must not be removed.
+
+        ---------------------------------------------------------------------
+
+
+"""
+
 import unittest
 
-from sppas import sppasTier, sppasLabel, sppasTag, sppasTypeError
-from sppas import sppasLocation, sppasPoint
-from sppas import sppasInterval
+from sppas import u
+from sppas import sppasTier, sppasLabel, sppasTag
+from sppas.src.anndata import sppasLocation, sppasPoint, sppasInterval
 from sppas.src.annotations.Occurrences.windowing import sppasTierWindow
+
+# ---------------------------------------------------------------------------
 
 
 class WindowingTest(unittest.TestCase):
@@ -13,26 +49,113 @@ class WindowingTest(unittest.TestCase):
     def setUp(self):
 
         # A sentence with 119 entries.
-        self.sent = "#" \
-               " David Lightman est plus intéressé par son ordinateur que par ses études +" \
-               " ce qui ne l' inquiète pas puisqu' il se sait capable de modifier lui-même +" \
-               " ses notes en agissant sur l' ordinateur de l'école" \
-               " # " \
-               " Un jour + " \
-               " en cherchant à percer le code d' accès d' un nouveau jeu vidéo +" \
-               " il se branche accidentellement +" \
-               " sur l'ordinateur du département de la Défense +" \
-               " qui prend au sérieux ce qui n' était au départ qu' un jeu" \
-               " #" \
-               " Pour les besoins d' une scène +" \
-               " Matthew Broderick s'est entraîné pendant deux mois +" \
-               " sur les jeux Galaxian et Galaga grâce à des bornes d' arcade +" \
-               " livrées directement chez lui par le studio" \
-               " #"   # (WarGames, 1983)
+        self.sent = u("#"
+               " David Lightman est plus intéressé par son ordinateur que par ses études +"
+               " ce qui ne l' inquiète pas puisqu' il se sait capable de modifier lui-même +"
+               " ses notes en agissant sur l' ordinateur de l'école"
+               " # "
+               " Un jour + "
+               " en cherchant à percer le code d' accès d' un nouveau jeu vidéo +"
+               " il se branche accidentellement +"
+               " sur l'ordinateur du département de la Défense +"
+               " qui prend au sérieux ce qui n' était au départ qu' un jeu"
+               " #"
+               " Pour les besoins d' une scène +"
+               " Matthew Broderick s'est entraîné pendant deux mois +"
+               " sur les jeux Galaxian et Galaga grâce à des bornes d' arcade +"
+               " livrées directement chez lui par le studio"
+               " #")   # (WarGames, 1983)
+
+    # -----------------------------------------------------------------------
+
+    def test_search(self):
+        """Test the search for the annotation set among a given interval.
+
+        """
+        # Create an Tier with localization = sppasIntervals, sppasPoint = int.
+        tier = sppasTier()
+        for i, entry in enumerate(self.sent.split()):
+            tier.create_annotation(
+                sppasLocation(sppasInterval(sppasPoint(i), sppasPoint(i+1))),
+                sppasLabel(sppasTag(entry)))
+
+        # Create the windowing class
+        wi = sppasTierWindow(tier)
+
+        # Test without and with tags to ignore
+        s = wi.search_for_annotations(0, 15, delta=0.5, ignore=[])
+        self.assertEqual(15, len(s))
+        s = wi.search_for_annotations(0, 15, delta=0.5, ignore=["#", "+"])
+        self.assertEqual(13, len(s))
+
+        # test delta on the last annotation
+        s = wi.search_for_annotations(0, 15.4, delta=0.5, ignore=[])
+        self.assertEqual(15, len(s))
+        s = wi.search_for_annotations(0, 15.5, delta=0.5, ignore=[])
+        self.assertEqual(16, len(s))
+        s = wi.search_for_annotations(0, 15.5, delta=0.7, ignore=[])
+        self.assertEqual(15, len(s))
+
+        # test delta on the first annotation
+        s = wi.search_for_annotations(0.6, 15, delta=0.5, ignore=[])
+        self.assertEqual(14, len(s))
+        s = wi.search_for_annotations(0.4, 15, delta=0.5, ignore=[])
+        self.assertEqual(15, len(s))
+
+        # test delta on both the first and the last annotation
+        s = wi.search_for_annotations(0.4, 15.6, delta=0.5, ignore=[])
+        self.assertEqual(16, len(s))
+
+    # -----------------------------------------------------------------------
+
+    def test_continuous_anchor_split(self):
+        """Test the search for all time intervals within a window given by separators.
+
+        """
+        # Create a Tier with localization = sppasInterval, with sppasPoint = int.
+        tier = sppasTier()
+        for i, entry in enumerate(self.sent.split()):
+            tier.create_annotation(
+                sppasLocation(sppasInterval(sppasPoint(i), sppasPoint(i + 1))),
+                sppasLabel(sppasTag(entry)))
+
+        # Create the windowing class
+        wi = sppasTierWindow(tier)
+
+        intervals = wi.continuous_anchor_split(["#"])
+        self.assertEqual(3, len(intervals))
+        self.assertEqual(1, intervals[0][0])
+        self.assertEqual(38, intervals[0][1])
+        self.assertEqual(39, intervals[1][0])
+        self.assertEqual(82, intervals[1][1])
+        self.assertEqual(83, intervals[2][0])
+        self.assertEqual(118, intervals[2][1])
+
+        #todo: Test if holes between the annotations of the tier
+
+        # Create a Tier with localization = sppasPoint = int.
+        tier = sppasTier()
+        for i, entry in enumerate(self.sent.split()):
+            tier.create_annotation(
+                sppasLocation(sppasPoint(i+1)),
+                sppasLabel(sppasTag(entry)))
+
+        # Create the windowing class
+        wp = sppasTierWindow(tier)
+
+        intervals = wp.continuous_anchor_split(["#"])
+        self.assertEqual(3, len(intervals))
+        self.assertEqual(1, intervals[0][0])
+        self.assertEqual(38, intervals[0][1])
+        self.assertEqual(39, intervals[1][0])
+        self.assertEqual(82, intervals[1][1])
+        self.assertEqual(83, intervals[2][0])
+        self.assertEqual(118, intervals[2][1])
 
     # -----------------------------------------------------------------------
 
     def test_time_split_int_points(self):
+        # Create a Tier with localization = sppasPoint = int.
         tier = sppasTier()
         for i, entry in enumerate(self.sent.split()):
             tier.create_annotation(
@@ -40,11 +163,10 @@ class WindowingTest(unittest.TestCase):
                 sppasLabel(sppasTag(entry)))
         wp = sppasTierWindow(tier)
 
-        # does not work as expected (11 words in each window)
         sp10 = wp.time_split(10, 10)
         self.assertEqual(12, len(sp10))
         for i in range(11):
-            self.assertEqual(10, len(sp10[i]))
+            self.assertEqual(11, len(sp10[i]))
         self.assertEqual(9, len(sp10[11]))
 
     # -----------------------------------------------------------------------
@@ -80,6 +202,8 @@ class WindowingTest(unittest.TestCase):
     # -----------------------------------------------------------------------
 
     def test_time_split_float_intervals(self):
+        # Create a Tier with localization = sppasInterval, with sppasPoint = float.
+
         tier = sppasTier()
         for i, entry in enumerate(self.sent.split()):
             tier.create_annotation(
@@ -129,46 +253,6 @@ class WindowingTest(unittest.TestCase):
 
     # -----------------------------------------------------------------------
 
-    def test_continuous_anchor_split_int_intervals(self):
-        tier = sppasTier()
-        for i, entry in enumerate(self.sent.split()):
-            tier.create_annotation(
-                sppasLocation(sppasInterval(sppasPoint(i), sppasPoint(i + 1))),
-                sppasLabel(sppasTag(entry)))
-        wi = sppasTierWindow(tier)
-
-        intervals = wi.continuous_anchor_split(["#"])
-        self.assertEqual(3, len(intervals))
-        self.assertEqual(1, intervals[0][0])
-        self.assertEqual(38, intervals[0][1])
-        self.assertEqual(39, intervals[1][0])
-        self.assertEqual(82, intervals[1][1])
-        self.assertEqual(83, intervals[2][0])
-        self.assertEqual(118, intervals[2][1])
-
-        #todo: Test if holes in the tier
-
-    # -----------------------------------------------------------------------
-
-    def test_continuous_anchor_split_int_points(self):
-        tier = sppasTier()
-        for i, entry in enumerate(self.sent.split()):
-            tier.create_annotation(
-                sppasLocation(sppasPoint(i+1)),
-                sppasLabel(sppasTag(entry)))
-        wp = sppasTierWindow(tier)
-
-        intervals = wp.continuous_anchor_split(["#"])
-        self.assertEqual(3, len(intervals))
-        self.assertEqual(1, intervals[0][0])
-        self.assertEqual(38, intervals[0][1])
-        self.assertEqual(39, intervals[1][0])
-        self.assertEqual(82, intervals[1][1])
-        self.assertEqual(83, intervals[2][0])
-        self.assertEqual(118, intervals[2][1])
-
-    # -----------------------------------------------------------------------
-
     def test_anchor_split_int_intervals(self):
         tier = sppasTier()
         for i, entry in enumerate(self.sent.split()):
@@ -178,7 +262,11 @@ class WindowingTest(unittest.TestCase):
         wi = sppasTierWindow(tier)
 
         si = wi.anchor_split(1, 1, ["#"])
-
+        self.assertEqual(3, len(si))
+        self.assertEqual(37, len(si[0]))
+        self.assertEqual(43, len(si[1]))
+        self.assertEqual(35, len(si[2]))
+        si = wi.anchor_split()
         self.assertEqual(3, len(si))
         self.assertEqual(37, len(si[0]))
         self.assertEqual(43, len(si[1]))
